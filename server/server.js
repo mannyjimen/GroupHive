68f74c9ed5a7859a468203ff474c1a52dcdc24f9
mannyjimen <manueljimenez0320@gmail.com>
2025-09-24 20:09:59 -0400

    can make POST user requests, stores user name and email (required) models

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 // server/server.js
 
 import express from 'express';
-import connectDB from './config/db.js'; // Notice the .js extension is often needed in ES Modules
+import connectDB from './config/db.js'; 
+import User from './models/User.js'
 
-// --- Initial Setup ---
-// Connect to the database
+//initial setup
 connectDB();
-
-// Initialize Express app
 const app = express();
 
 // --- Middleware ---
@@ -15,16 +13,42 @@ const app = express();
 app.use(express.json());
 
 // --- Routes ---
-// A simple test route to make sure the server is working
 app.get('/', (req, res) => {
   res.send('API is running successfully!');
 });
 
-// --- Server Listening ---
-// Get the port from environment variables, with a default of 5000
-const PORT = process.env.PORT || 5000;
+app.post('/api/users', async (req,res) => {
+  try {
+    //get name and email from request body
+    const { name, email } = req.body;
 
-// Start the server
+    //check if user already exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists'});
+    }
+
+    //create new user instance from model
+    const user = new User({
+      name,
+      email,
+    });
+
+    const createdUser = await user.save();
+
+    //send the newly created user back as a response
+    res.status(201).json(createdUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error'});
+  }
+});
+
+
+
+// --- Server Listening ---
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
