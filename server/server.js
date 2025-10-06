331ce7a0e45ad936e03bc659dd75f1109636f4b0
mannyjimen <manueljimenez0320@gmail.com>
2025-10-05 22:20:32 -0400

    added events struct (which implicitly added the collection in mongoDB)

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import connectDB from './config/db.js'; 
 import User from './models/User.js'
+import Event from './models/Events.js'
 
 //initial setup
 connectDB();
@@ -27,6 +28,23 @@ app.get('/api/users', async (req, res) => {
     // 3. Handle errors if the database query fails
     console.error(error);
     res.status(500).json({ message: 'Error retrieving users from database', error: error.message });
+  }
+});
+
+app.get('/api/events', async (req, res) => {
+    try {
+
+    //retrieve all docs in Events collection
+    const events = await Event.find({});
+
+    // 2. Send the retrieved data (users) as a JSON response
+    // The status 200 (OK) is implicit but good practice to include
+    res.status(200).json(events);
+
+  } catch (error) {
+    // 3. Handle errors if the database query fails
+    console.error(error);
+    res.status(500).json({ message: 'Error retrieving events from database', error: error.message });
   }
 });
 
@@ -57,6 +75,40 @@ app.post('/api/users', async (req,res) => {
     res.status(500).json({ message: 'Server Error'});
   }
 });
+
+app.post('/api/events', async (req,res) => {
+  try {
+    //get name and email from request body
+    const { name, category, description, location, numberPeople } = req.body;
+
+    //check if user already exists
+    const eventExists = await Event.findOne({ name });
+
+    if (eventExists) {
+      return res.status(400).json({ message: 'Event already exists'});
+    }
+
+    //create new user instance from model
+    const event = new Event({
+      name,
+      category,
+      description,
+      location,
+      numberPeople
+    });
+
+    console.log("created event")
+
+    const createdEvent = await event.save();
+
+    //send the newly created user back as a response
+    res.status(201).json(createdEvent);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error'});
+  }
+});
+
 
 
 
