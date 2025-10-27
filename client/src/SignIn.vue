0bee8d23abb983da65ac2abbbef04bb4bbcf218d
Eric Lin <eric.lin67@myhunter.cuny.edu>
2025-10-26 21:37:16 -0400

    Sign-Up functional, still working on Sign-In

--- client/src/SignIn.vue
+++ client/src/SignIn.vue
@@ -1,22 +1,50 @@
 <script setup lang="ts">
     import { ref } from 'vue'
     import { createApp } from 'vue'
+    import axios from 'axios'
+    import { useRouter } from 'vue-router'
 
-    const user = ref('') //Value would be saved to user
-    const pass = ref('') //Value would be saved to pass
-    function onClick() {
-        this.$root.$emit('loggingIn');
-        //Function does not work will check later
-        //1. Sign in
-        //2. Go to search page
+    const username = ref('') //Value would be saved to user
+    const password = ref('') //Value would be saved to pass
+    const error = ref('') // 3. Add an error ref
+
+    const router = useRouter() // 4. Get the router instance
+
+    // 5. Replace the onClick function
+    async function onClick() {
+        error.value = ''
+        try {
+            // 6. Call your backend's login route
+            const response = await axios.post('http://localhost:5000/api/login', {
+                username: username.value,     // 'user' from form maps to 'name' in API
+                password: password.value  // 'pass' from form maps to 'password' in API
+            });
+
+            // 7. Get the token from the response
+            const token = response.data.token;
+
+            // 8. CRITICAL: Store the token in the browser
+            localStorage.setItem('token', token);
+
+            // 9. Redirect to the search page (as you wanted)
+            //router.push('/search'); // <-- This will navigate to your search page - still working on what page to direct to
+
+        } catch (err: any) {
+            // 10. Handle "Invalid credentials"
+            if (err.response && err.response.data.message) {
+                error.value = err.response.data.message;
+            } else {
+                error.value = 'An error occurred during sign in.';
+            }
+        }
     }
 </script>
 
 <template>
     <div class="form">
         <div class="signIn">Sign In</div>
-        <input class="user" v-model="user" placeholder="Username">
-        <input class="pass" v-model="pass" placeholder="Password">
+        <input class="user" v-model="username" placeholder="Username">
+        <input class="pass" v-model="password" placeholder="Password">
         <button class="button" @click="onClick">Sign In</button>
         <!-- To see value
         <p>{{ user }}</p>
